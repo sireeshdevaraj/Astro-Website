bece2dd33094ee8e22a25950145588b22b753fae
sireeshdevaraj <kongarasireeshdevaraj@gmail.com>
2024-07-11 01:44:31 +0530

    added skills and names

--- src/consts.ts
+++ src/consts.ts
@@ -6,7 +6,7 @@ export const ABOUT_1 = `
 I'm a recreational programmer with extensive experience in research and development. Over the past years, I have worked as a freelance software engineer, completed a 6-month internship, and am currently working as a software engineer.`
 export const ABOUT_2 = `I am proficient in Go, Javascript, Python, R, and Vensim for system modeling. My database expertise includes MySQL and PostgreSQL.
 Additionally, I have strong deployment skills with NGINX, pm2, and Docker. Worked with AWS, SES and also google analytics.`
-export const ABOUT_3 = `I'm actively looking for roles(backend or frontend) where I can work on some challenging projects!!`
+export const ABOUT_3 = `Challenging roles in backend or frontend development.`
 export const ABOUT_LOVE= `I love spending time to build random software and reading books.`
 export const GAMING_ABOUT = "I play a lot of games just like Sora from no game no life, I mostly play World of Warcraft, Valorant and OSU(To improve aim and reflex)"
 export const VALORANT = {
@@ -21,3 +21,39 @@ export const ANIME = data
 export const PROFILE_PICTURE = "/images/profile_pics/boruto1.jpeg"
 export const EMOJIS = ['🥶','🧊','❄️','☃️','⛄']
 export const DATE = new Date().toLocaleDateString("en-US",{ year: 'numeric', month: 'long', day: 'numeric' })
+export const NAMES = [
+    'Sireesh',   // English, Spanish, French, German, Portuguese, Italian
+    '시리쉬',     // Korean
+    'シリーシュ', // Japanese
+    '西瑞什',     // Chinese
+    'سيريش',     // Arabic
+    'Сириш',     // Russian
+
+];
+export const SKILLS = {
+    "languages" : [ // Image Array of programming languages
+            "/images/skills/js.png",
+            "/images/skills/go.png",
+            "/images/skills/python.png",
+            "/images/skills/R.png"
+
+    ], 
+    "databases" : [
+        "/images/skills/mysql.png",
+        "/images/skills/psql.png"
+
+    ], 
+    "modelling" : ["/images/skills/vensim.png"
+    ],   
+    "deployment": [
+        "/images/skills/docker.png",
+        "/images/skills/pm2.png",
+        "/images/skills/nginx.png"
+    ],
+    "cloud"     : [
+        "/images/skills/aws.png",
+        "/images/skills/ses.png"
+
+    ]
+
+}
