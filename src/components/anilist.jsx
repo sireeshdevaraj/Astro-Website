fcc189bfff827e8558b9d4d520770e4101202783
sireeshdevaraj <kongarasireeshdevaraj@gmail.com>
2024-07-11 01:53:11 +0530

    changed styles for anilist

--- src/components/anilist.jsx
+++ src/components/anilist.jsx
@@ -13,7 +13,7 @@ export default function Anilist(){
         setAnilist(data);
     },[])
     return <div>
-        <p>As of {DATE}, I have watched over {ANIME.userStat.episodes} episodes and {ANIME.userStat.watched} animes.(Tracked by <a href="https://anilist.co/user/KuuHaKu7/">Anilist</a>)</p>
+        <p className="tw-text-gray-600 tw-text-lg">As of {DATE}, I have watched over {ANIME.userStat.episodes} episodes and {ANIME.userStat.watched} animes.(Tracked by <a className="hover:tw-text-purple-600 tw-cursor-pointer" href="https://anilist.co/user/KuuHaKu7/">Anilist</a>)</p>
             <Note content={ANIME.activityStat.status.toUpperCase() + " " + ANIME.activityStat.media.title.english} header={"Recently Watched/Completed"} />
         </div> 
 }
