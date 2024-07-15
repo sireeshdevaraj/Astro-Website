c17fe442826f2de9bc650927e6ca972e830f646d
sireeshdevaraj <kongarasireeshdevaraj@gmail.com>
2024-07-16 01:03:52 +0530

    UPDATE: add new anilist URL and frontend manipulation

--- src/api.anime.js
+++ src/api.anime.js
@@ -1,3 +1,3 @@
-let animeData = await fetch("https://api.kuuhaku.space/anilist")
+let animeData = await fetch("https://api.kuuhaku.space/anilist/5631742")
 let result = await animeData.json()
 export const data = result
--- src/components/anilist.jsx
+++ src/components/anilist.jsx
@@ -1,15 +1,13 @@
 "use strict";
-import {useState,useEffect} from "react";
 import {
     DATE,
     ANIME
-} from "../consts"
-import {data} from "../api.anime";
+} from "../consts";
 import Note from "./note";
 
 export default function Anilist(){
     return <div>
-        <p className="tw-text-gray-600 tw-text-lg">As of {DATE}, I have watched over {ANIME.userStat.episodes} episodes and {ANIME.userStat.watched} animes.(Tracked by <a className="hover:tw-text-purple-600 tw-cursor-pointer" href="https://anilist.co/user/KuuHaKu7/">Anilist</a>)</p>
-            <Note content={ANIME.activityStat.status.toUpperCase() + " " + ANIME.activityStat.media.title.english} header={"Recently Watched/Completed"} />
+        <p className="tw-text-gray-600 tw-text-lg">As of {DATE}, I have watched over {ANIME.user.statistics.anime.episodesWatched} episodes and {ANIME.user.statistics.anime.count} animes.(Tracked by <a className="hover:tw-text-purple-600 tw-cursor-pointer" href="https://anilist.co/user/KuuHaKu7/">Anilist</a>)</p>
+            <Note content={ANIME.mediaListCollection.lists[0].entries[0].status + " " + ANIME.mediaListCollection.lists[0].entries[0].media.title.english} header={"Progress:" + ANIME.mediaListCollection.lists[0].entries[0].progress + "eps"} />
         </div> 
 }
