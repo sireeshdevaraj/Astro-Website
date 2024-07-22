6d2c2a5d984f0798bb51e6c506349925676bb48c
sireeshdevaraj <kongarasireeshdevaraj@gmail.com>
2024-07-23 01:57:05 +0530

    ADD: dark mode for the main page
    Need to change the final primary white color code for the website

--- src/components/blogForMainPage.jsx
+++ src/components/blogForMainPage.jsx
@@ -1,11 +1,11 @@
 export default function BlogForMainPage(props) {
     let {posts} = props
     return (
-      <div className="tw-bg-white tw-py-8 mobile:tw-py-8 tw-mt-3">
+      <div className="tw-py-8 mobile:tw-py-8 tw-mt-3">
         <div className="tw-mx-auto tw-max-w-7xl">
           <div className="tw-mx-auto tw-max-w-2xl desktop:tw-mx-0">
-            <h2 className="tw-text-3xl tw-font-bold tw-tracking-tight tw-text-gray-900 mobile:tw-text-4xl">From the blog</h2>
-            <p className="tw-mt-2 tw-text-lg tw-leading-8 tw-text-gray-600">
+            <h2 className="tw-text-3xl tw-font-bold tw-tracking-tight tw-text-gray-900 mobile:tw-text-4xl dark:tw-text-darkHeading">From the blog</h2>
+            <p className="tw-mt-2 tw-text-lg tw-leading-8 tw-text-gray-600 dark:tw-text-darkPara">
             Check out my latest blogs! For the full scoop, head over to the Blogs section and dive into all the madness (if you can find any, haha).
             </p>
           </div>
@@ -13,24 +13,24 @@ export default function BlogForMainPage(props) {
             {posts.map((post) => (
               <article key={post.id} className="tw-flex tw-max-w-xl tw-flex-col tw-items-start">
                 <div className="tw-flex tw-items-center tw-gap-x-4 tw-text-xs">
-                  <time dateTime={post.data.pubDate.toLocaleDateString('en-us', {year: 'numeric',month: 'short',day: 'numeric'})} className="text-gray-500">
+                  <time dateTime={post.data.pubDate.toLocaleDateString('en-us', {year: 'numeric',month: 'short',day: 'numeric'})} className="text-gray-500 dark:tw-text-darkPara2">
                     {post.data.pubDate.toLocaleDateString('en-us', {year: 'numeric',month: 'short',day: 'numeric'})}
                   </time>
                   <a
                     // href={post.data?.category || "random"}
-                    className="tw-cursor-default tw-text-black tw-relative tw-rounded-full tw-bg-gray-50 tw-px-3 tw-py-1.5 tw-font-medium tw-text-gray-600 hover:tw-bg-gray-100"
+                    className="tw-cursor-default tw-text-black tw-relative tw-rounded-full tw-bg-gray-50 dark:tw-bg-gray-600 dark:tw-text-darkPara dark:hover:tw-bg-white dark:hover:tw-text-black tw-px-3 tw-py-1.5 tw-font-medium tw-text-gray-600 hover:tw-bg-gray-100"
                   >
                     {post.data.category || "rant" }
                   </a>
                 </div>
                 <div className="tw-group tw-relative">
                   <h3 className="tw-mt-3 tw-text-lg tw-font-semibold tw-leading-6 tw-text-gray-900 group-hover:tw-text-gray-600 tw-h-7">
-                    <a href={`/blog/${post.slug}/`} className="tw-text-black tw-text-black hover:tw-text-gray-600 tw-no-underline">
+                    <a href={`/blog/${post.slug}/`} className="tw-text-black tw-text-black hover:tw-text-gray-600 tw-no-underline dark:tw-text-darkHeading">
                       <span className="tw-absolute tw-inset-0" />
                       {post.data.title}
                     </a>
                   </h3>
-                  <p className="tw-mt-5 tw-line-clamp-3 tw-text-sm tw-leading-6 tw-text-gray-600">{post.data.description}</p>
+                  <p className="tw-mt-5 tw-line-clamp-3 tw-text-sm tw-leading-6 tw-text-gray-600 dark:tw-text-darkPara">{post.data.description}</p>
                 </div>
               </article>
             ))}
--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -2,7 +2,7 @@ import { SKILLS } from "../consts";
 export default function Skills(){
     return <div className="tw-mb-4 tw-ring-1 tw-ring-gray-900/5 tw-p-4 mobile:tw-w-fit mobile:tw-flex-col tablet:tw-w-80 desktop:tw-w-full desktop:tw-grid desktop:tw-grid-cols-2">
         <div className="tw-flex tw-text-center tw-inline-flex tw-items-center tw-ml-2 tw-p-2 tw-h-8">
-            <p className="tw-font-berkeley tw-text-gray-600 tw-text-lg">Languages:</p>
+            <p className="tw-font-berkeley tw-text-gray-600 tw-text-lg dark:tw-text-darkPara">Languages:</p>
             {
             SKILLS.languages.map((value,index) => {
                 return <img className="tw-size-12 tw-object-contain hover:tw-scale-125 hover:tw-transition tw-ease-in-out hover:tw-scale-125 hover:tw-transition tw-ease-in-out" src={value}/>
@@ -10,7 +10,7 @@ export default function Skills(){
             }
         </div>
         <div className="tw-flex tw-text-center tw-inline-flex tw-items-center tw-ml-2 tw-p-2 tw-h-8">
-            <p className="tw-font-berkeley tw-text-gray-600 tw-text-lg">Databases:</p>
+            <p className="tw-font-berkeley tw-text-gray-600 tw-text-lg dark:tw-text-darkPara">Databases:</p>
             {
             SKILLS.databases.map((value,index) => {
                 return <img className="tw-size-12 tw-object-contain hover:tw-scale-125 hover:tw-transition tw-ease-in-out" src={value}/>
@@ -18,7 +18,7 @@ export default function Skills(){
             }
         </div>
         <div className="tw-flex tw-text-center tw-inline-flex tw-items-center tw-ml-2 tw-p-2 tw-h-8">
-            <p className="tw-font-berkeley tw-text-gray-600 tw-text-lg">System modelling:</p>
+            <p className="tw-font-berkeley tw-text-gray-600 tw-text-lg dark:tw-text-darkPara">System modelling:</p>
             {
             SKILLS.modelling.map((value,index) => {
                 return <img className="tw-size-12 tw-object-contain hover:tw-scale-125 hover:tw-transition tw-ease-in-out" src={value}/>
@@ -26,7 +26,7 @@ export default function Skills(){
             }
         </div>
         <div className="tw-flex tw-text-center tw-inline-flex tw-items-center tw-ml-2 tw-p-2 tw-h-8">
-            <p className="tw-font-berkeley tw-text-gray-600 tw-text-lg">Deployment:</p>
+            <p className="tw-font-berkeley tw-text-gray-600 tw-text-lg dark:tw-text-darkPara">Deployment:</p>
             {
             SKILLS.deployment.map((value,index) => {
                 return <img className="tw-size-12 tw-object-contain hover:tw-scale-125 hover:tw-transition tw-ease-in-out" src={value}/>
@@ -34,7 +34,7 @@ export default function Skills(){
             }
         </div>
         <div className="tw-flex tw-text-center tw-inline-flex tw-items-center tw-ml-2 tw-p-2 tw-h-8">
-            <p className="tw-font-berkeley tw-text-gray-600 tw-text-lg">Cloud:</p>
+            <p className="tw-font-berkeley tw-text-gray-600 tw-text-lg dark:tw-text-darkPara">Cloud:</p>
             {
             SKILLS.cloud.map((value,index) => {
                 return <img className="tw-size-12 tw-object-contain hover:tw-scale-125 hover:tw-transition tw-ease-in-out" src={value}/>
