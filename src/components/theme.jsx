42b495f6dcb0cd852933b00ef9bd099c45ce1f27
sireeshdevaraj <kongarasireeshdevaraj@gmail.com>
2024-07-23 00:53:04 +0530

    START: Dark mode addition

---
+++ src/components/theme.jsx
@@ -0,0 +1,16 @@
+import { theme } from "../state.store";
+import { useState, useEffect} from "react";
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
+
+export default function Theme(){
+    const [dark,setDark] = useState(theme.get())
+    
+    const handleThemeClick = () => theme.get() ? setDark(false) : setDark(true)
+
+    useEffect(()=>{
+        theme.set(dark)
+        dark ? document.documentElement.classList.add("tw-dark") : document.documentElement.classList.remove("tw-dark") 
+    },[dark])
+    return <div className="hover:tw-cursor-pointer" onClick={handleThemeClick}>{dark ? <DarkModeIcon/> : <LightModeIcon />}</div>
+}
--- src/state.store.js
+++ src/state.store.js
@@ -4,3 +4,9 @@ export const survey = persistentAtom("isSurveyCompleted",false,{
     encode: JSON.stringify,
     decode: JSON.parse,
   });
+
+export const theme = persistentAtom("dark",false,{
+  // false for dark
+  encode: JSON.stringify,
+  decode: JSON.parse
+})
