cfdf0ae04b54fb454d0128a2050ca1cff83d5d52
sireeshdevaraj <kongarasireeshdevaraj@gmail.com>
2024-07-27 23:42:45 +0530

    Refactor: date

--- src/components/anilist.jsx
+++ src/components/anilist.jsx
@@ -1,13 +1,13 @@
 "use strict";
 import {
-    DATE,
     ANIME
 } from "../consts";
 import Note from "./note";
 
 export default function Anilist(){
+    const date = new Date().toLocaleDateString("en-US",{ year: 'numeric', month: 'long', day: 'numeric' })
     return <div>
-        <p className="dark:tw-text-darkPara tw-text-gray-600 tw-text-lg">As of {DATE}, I have watched over {ANIME.user.statistics.anime.episodesWatched} episodes and {ANIME.user.statistics.anime.count} animes.(Tracked by <a className="hover:tw-text-purple-600 tw-cursor-pointer tw-font-sriracha" href="https://anilist.co/user/KuuHaKu7/">Anilist</a>)</p>
+        <p className="dark:tw-text-darkPara tw-text-gray-600 tw-text-lg">As of {date}, I have watched over {ANIME.user.statistics.anime.episodesWatched} episodes and {ANIME.user.statistics.anime.count} animes.(Tracked by <a className="hover:tw-text-purple-600 tw-cursor-pointer tw-font-sriracha" href="https://anilist.co/user/KuuHaKu7/">Anilist</a>)</p>
             <Note content={ANIME.mediaListCollection.lists[0].entries[0].status + " " + ANIME.mediaListCollection.lists[0].entries[0].media.title.english} header={"Progress:" + ANIME.mediaListCollection.lists[0].entries[0].progress + "eps"} />
         </div> 
 }
