5d6f71c53b247f8f67d2d24c77815424515e908f
sireeshdevaraj <kongarasireeshdevaraj@gmail.com>
2024-10-06 00:58:07 -0400

    Intersection observer implementation

---
+++ src/scripts/observer.js
@@ -0,0 +1,18 @@
+const intersectionObserver = new IntersectionObserver((entries,observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting){
+          entry.target.style.animation = "fadeIn 1s ease-in-out 300ms";
+          setTimeout(() => {
+            entry.target.style.visibility = "visible";
+        },500);
+      } 
+    }) 
+  });
+
+intersectionObserver.observe(document.getElementById("about"));	
+intersectionObserver.observe(document.getElementById("profile_image"));	
+intersectionObserver.observe(document.getElementById("simulations_container"));	
+intersectionObserver.observe(document.getElementById("publications"));	
+intersectionObserver.observe(document.getElementById("skills"));	
+intersectionObserver.observe(document.getElementById("blogForMainPage"));	
+intersectionObserver.observe(document.getElementById("gaming"));	
